f960b2f8cd517d27db840c819e59cf548c54876a
Pawel Lekki <pawel.lekki@gmail.com>
2015-03-06 11:01:35 +0100

    thought support

--- lib/mongoid/factory.rb
+++ lib/mongoid/factory.rb
@@ -7,7 +7,7 @@ module Mongoid
 
     def replace_type(type)	
 
-      if ['Checkin', 'FoursquareItem', 'InstagramCheckinItem', 'FacebookCheckinItem', 'FacebookImageCheckinItem', 'Text'].include?(type)   
+      if ['Checkin', 'FoursquareItem', 'InstagramCheckinItem', 'FacebookCheckinItem', 'FacebookImageCheckinItem', 'Text', 'Thought'].include?(type)   
         'Mongo'+type
       else
         type
