6cba5b454d9ca91b91d3445879cbd8fe5773b01b
Pawel Lekki <pawel.lekki@gmail.com>
2015-03-09 02:07:57 +0100

    insta image support

--- lib/mongoid/factory.rb
+++ lib/mongoid/factory.rb
@@ -8,7 +8,7 @@ module Mongoid
     def replace_type(type)	
 
       if ['Checkin', 'FoursquareItem', 'InstagramCheckinItem', 'FacebookCheckinItem', 'FacebookImageCheckinItem', 
-          'Text', 'Thought', 'FacebookStatusItem', 'TwitterItem', 'Image', 'TwitterImageItem', 'DropboxImageItem', 'InstagramImageItem'].include?(type)   
+          'Text', 'Thought', 'FacebookStatusItem', 'TwitterItem', 'Image', 'TwitterImageItem', 'DropboxImageItem', 'InstagramItem'].include?(type)   
         'Mongo'+type
       else
         type
